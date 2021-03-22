08032c75df0d792f0e45bd8a5dfa35aae888ad09
Robiullah2244 <robiullah2244@gmail.com>
2021-03-23 05:41:37 +0600

    Default TextInput and container Style added

---
+++ expense_tracker/src/style/Styles.js
@@ -0,0 +1,21 @@
+
+import{
+    StyleSheet
+
+} from 'react-native';
+
+export default Styles = StyleSheet.create({
+    DefaultTextInput:
+    {
+        height: 40,
+        borderColor: '#2383F7',
+        borderWidth: 1.5,
+        borderRadius: 8,
+        paddingHorizontal: 8,
+        margin: 10,
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+})
