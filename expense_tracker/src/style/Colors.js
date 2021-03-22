baae519afb4f24e13ae8afcf9cde85b82884d8fa
Robiullah2244 <robiullah2244@gmail.com>
2021-03-23 05:42:57 +0600

    Some common color code added to a js file

---
+++ expense_tracker/src/style/Colors.js
@@ -0,0 +1,12 @@
+
+export const PrimaryBlue = '#007aff';
+
+export const Blue = '#379fff';
+export const White = 'white';
+
+export const Black = 'black';
+export const Red = '#ff696a';
+export const Yellow = '#ffff00';
+
+// export const Green = '#159c4d';
+export const BlueWithOpacity = 'rgba(0, 123, 255, 0.3)';
