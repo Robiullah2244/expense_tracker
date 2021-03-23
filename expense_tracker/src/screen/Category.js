1efe5261ad9a224a28d3e2b590de8af9cbe7e3fb
Robiullah2244 <robiullah2244@gmail.com>
2021-03-23 06:45:18 +0600

    Category screen Basic Ui Done

--- expense_tracker/src/screen/Category.js
+++ expense_tracker/src/screen/Category.js
@@ -1,16 +1,116 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View, Modal, KeyboardAvoidingView, TextInput, ScrollView, StyleSheet, FlatList } from 'react-native'
 import Header from 'expense_tracker/src/component/Header';
+import Button from 'expense_tracker/src/component/Button';
+import Background from 'expense_tracker/src/component/Background';
+import {PrimaryBlue, Gray, Green} from 'expense_tracker/src/style/Colors';
+import Styles from 'expense_tracker/src/style/Styles';
 
 export default class Category extends Component {
-    render() {
-        return (
-            <View>
-                <Header headerText='Category' navigation={this.props.navigation}/>
-                <View>
-                    <Text> Category Screen </Text>
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            modalVisible: false,
+            category: '',
+        }
+    }
+
+    renderItem = (item, index) => {
+        console.log(item);
+        return(
+            <View style={{marginTop: 8, marginHorizontal: 12,}}>
+                <View
+                    style={{
+                    borderBottomColor: Gray,
+                    borderBottomWidth: 1.5,
+                    }}
+                />
+
+                <View style={{marginTop: 8}}>
+                    <View style={{flexDirection: "row", justifyContent: "space-between",}}>
+                        <Text style={{fontWeight: "bold", fontSize: 16}}>{index+1}</Text>
+                        <Text style={{fontWeight: "bold", fontSize: 16}}>{item.id}</Text>
+                    </View>
                 </View>
             </View>
+        );
+    };
+    
+    render() {
+        return (
+            <>
+                <Header headerText='Category' navigation={this.props.navigation}/>
+                <Background>
+                    <View>
+                        <View style={{flexDirection: 'row',}}>
+                            {/* <Button type='primary' title='Add New Category' width={150}/> */}
+                            <TextInput 
+                                style={[Styles.DefaultTextInput, {width: 160, height: 36}]}
+                                placeholder="Category"
+                                onChangeText={text => this.setState({category: text})}
+                                keyboardType="numeric"
+                            />
+                            <Button
+                                onPress={() => {this.setState({modalVisible: !this.state.modalVisible})}}
+                                title='Add New Category'
+                                buttonStyle={{backgroundColor: PrimaryBlue, width: 136, height: 36, borderRadius: 8}}
+                                titleStyle={{color: 'white', fontSize: 14, fontWeight: 'bold',}}
+                            />
+                        </View>
+
+                    </View>
+
+                    <View style={styles.flatListContainer}>
+                        <ScrollView>
+                            <FlatList
+                                data={[{'id': 1}, {'id': 2}]}
+                                // data={this.state.filteredInvoices}
+                                renderItem={({item, index}) => this.renderItem(item, index)}
+                                keyExtractor={item => item.id.toString()}
+                                ItemSeparatorComponent={() => (
+                                    <View style={{marginVertical: 4}} />
+                                )}
+                                ref={ref => {
+                                    this.flatListRef = ref;
+                                }}
+                                initialNumToRender={8}
+                            />
+                        </ScrollView>
+                    </View>
+
+                </Background>
+            </>
         )
     }
 }
+
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  inputWrap: {
+    flex: 1,
+    borderColor: '#cccccc',
+    borderBottomWidth: 1,
+    marginBottom: 10,
+    borderWidth: 0,
+    borderRadius: 8,
+    height: 36,
+    flex: 0.4875,
+    //   padding:10
+  },
+  input: {
+    fontSize: 14,
+    marginBottom: -15,
+    color: '#cccccc',
+  },
+  flatListContainer: {
+    marginTop: 16,
+    flex: 1,
+    // paddingBottom: 8
+  },
+});
