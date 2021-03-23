3d4057d251191c87094788de0023db3f8b72d8d2
Robiullah2244 <robiullah2244@gmail.com>
2021-03-23 06:46:08 +0600

    Some Colors added

--- expense_tracker/src/style/Colors.js
+++ expense_tracker/src/style/Colors.js
@@ -1,12 +1,20 @@
 
 export const PrimaryBlue = '#007aff';
 
+
 export const Blue = '#379fff';
 export const White = 'white';
 
 export const Black = 'black';
 export const Red = '#ff696a';
+export const BackgroundColor = '#e5eff9';
+export const Gold = '#FFD700';
 export const Yellow = '#ffff00';
+
+// export const Red = 'red';
+export const Gray = '#666666';
+export const SteelGray = '#222133';
 
 // export const Green = '#159c4d';
 export const BlueWithOpacity = 'rgba(0, 123, 255, 0.3)';
+export const GrayWithOpacity = 'rgba(102, 102, 102, 0.1)';
